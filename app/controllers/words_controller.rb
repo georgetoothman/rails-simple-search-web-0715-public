2d25d45ecc45fb0baeea93af99aaa57691bf0b87
George Toothman <george.toothman@flatironschool.com>
2015-09-07 10:14:11 -0400

    add simple search to mvc

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,3 +1,13 @@
 class WordsController < ApplicationController
+  def create
+    word = Search.for(params[:keyword])
+    if word == word
+      redirect_to "/words/#{word[0].id}"
+    else
+      flash[:notice] = "No results matching that query."
+    end
+  end
 
+  def show
+  end
 end
